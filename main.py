6a3ea9f028e31464ab0953d54735063a46303a18
yuanyu1031 <129406409+yuanyu1031@users.noreply.github.com>
2023-06-04 23:46:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -32,14 +32,5 @@ json_data = {
 }
 if __name__ == "__main__":
     cookie_str1 = os.environ.get("COOKIE1", None)
-    cookie_str2 = os.environ.get("COOKIE2", None)
-    cookie_str3 = os.environ.get("COOKIE3", None)
     cookies1 = ParseCookiestr(cookie_str1)
-    cookies2 = ParseCookiestr(cookie_str2)
-    cookies3 = ParseCookiestr(cookie_str3)
     response1 = requests.post('https://glados.network/api/user/checkin', cookies=cookies1, headers=headers, json=json_data)
-    response2 = requests.post('https://glados.network/api/user/checkin', cookies=cookies2, headers=headers, json=json_data)
-    response3 = requests.post('https://glados.network/api/user/checkin', cookies=cookies3, headers=headers, json=json_data)
-    cookie_str4 = os.environ.get("COOKIE4", None)
-    cookies4 = ParseCookiestr(cookie_str4)
-    response4 = requests.post('https://glados.network/api/user/checkin', cookies=cookies4, headers=headers, json=json_data)
